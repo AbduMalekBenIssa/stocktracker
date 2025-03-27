9a0b65bfcfe91d11d1d18627f7bd63e28b839eb9
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-03-26 23:28:38 -0600

    added in java doc for intro

--- stocktrackerproject-master/src/main/java/stocktracker/model/BuyTransaction.java
+++ stocktrackerproject-master/src/main/java/stocktracker/model/BuyTransaction.java
@@ -1,4 +1,12 @@
 package stocktracker.model;
 
+/**
+ * Class representing a buy transaction
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 2.0
+ * @Tutorial T04
+ */
+
 public class BuyTransaction {
 }
