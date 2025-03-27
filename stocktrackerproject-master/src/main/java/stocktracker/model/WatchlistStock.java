ad77328e8f21f6931af3bcfcd8659c1c87e28bdd
Omar Almishri <omaralmishri56@gmail.com>
2025-03-27 02:27:42 -0600

    watchliststock extending stock

--- stocktrackerproject-master/src/main/java/stocktracker/model/WatchlistStock.java
+++ stocktrackerproject-master/src/main/java/stocktracker/model/WatchlistStock.java
@@ -1,4 +1,16 @@
 package stocktracker.model;
 
-public class WatchlistStock {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+/**
+ * Class representing a stock in the user's watchlist
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 2.0
+ * @Tutorial T04
+ */
+
+public class WatchlistStock extends Stock {
 }
