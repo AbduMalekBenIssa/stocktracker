ec48e8bd4b814b4a055ccd86ee227dda1d701853
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-03-27 01:45:01 -0600

    added in imports and java doc for into, tutorial name and such etc

--- stocktrackerproject-master/src/main/java/stocktracker/tests/PortfolioTest.java
+++ stocktrackerproject-master/src/main/java/stocktracker/tests/PortfolioTest.java
@@ -1,4 +1,21 @@
 package stocktracker.tests;
 
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import stocktracker.model.OwnedStock;
+import stocktracker.model.Portfolio;
+
+
+/**
+ * JUnit tests for Portfolio class
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 2.0
+ * @Tutorial T04
+ */
 public class PortfolioTest {
 }
