df7c0cddb126ba2d7ddf807d4c219fbbb3721f15
Omar Almishri <omaralmishri56@gmail.com>
2025-03-27 01:48:55 -0600

    initializing user class

--- stocktrackerproject-master/src/main/java/stocktracker/model/User.java
+++ stocktrackerproject-master/src/main/java/stocktracker/model/User.java
@@ -1,4 +1,20 @@
 package stocktracker.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    /**
+     * Class representing a user of the stock tracker application
+     *
+     * @author Omar Almishri, AbduMalek Ben Issa
+     * @version 2.0
+     * @Tutorial T04
+     */
+    public class User {
+        private String name;
+        private double balance;
+        private Portfolio portfolio;
+        private Watchlist watchlist;
+        private List<Transaction> transactions;
 }
