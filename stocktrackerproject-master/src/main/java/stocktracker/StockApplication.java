763d0cddddb7a1e88e0674724f33c40de2a5ffbc
Omar Almishri <omaralmishri56@gmail.com>
2025-03-27 03:09:10 -0600

    initializing and importing StockApplication calss

--- stocktrackerproject-master/src/main/java/stocktracker/StockApplication.java
+++ stocktrackerproject-master/src/main/java/stocktracker/StockApplication.java
@@ -1,4 +1,22 @@
 package stocktracker;
 
+import stocktracker.model.*;
+import stocktracker.service.*;
+import stocktracker.util.*;
+import stocktracker.analysis.*;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
 public class StockApplication {
+    /**
+     * Main class for the Stock Tracker Application
+     *
+     * @author Omar Almishri, AbduMalek Ben Issa
+     * @version 2.0
+     * @Tutorial T04
+     */
+
 }
