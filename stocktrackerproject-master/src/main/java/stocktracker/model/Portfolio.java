de155b19829af6ba548e9eb99adf9171ba41d063
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-03-26 23:53:37 -0600

    added in info for next section

--- stocktrackerproject-master/src/main/java/stocktracker/model/Portfolio.java
+++ stocktrackerproject-master/src/main/java/stocktracker/model/Portfolio.java
@@ -1,4 +1,11 @@
 package stocktracker.model;
 
+/**
+ * Class representing a user's stock portfolio
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 2.0
+ * @Tutorial T04
+ */
 public class Portfolio {
 }
