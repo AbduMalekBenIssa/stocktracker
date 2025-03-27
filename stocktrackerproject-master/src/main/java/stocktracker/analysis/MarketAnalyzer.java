807639847c59048a5e0c1d3c09ba823f55915732
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-03-27 00:31:15 -0600

    added imports and java doc for intro

--- stocktrackerproject-master/src/main/java/stocktracker/analysis/MarketAnalyzer.java
+++ stocktrackerproject-master/src/main/java/stocktracker/analysis/MarketAnalyzer.java
@@ -1,4 +1,21 @@
 package stocktracker.analysis;
 
+
+import stocktracker.service.StockMarket;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Class for analyzing market data and providing advanced insights
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 2.0
+ * @Tutorial T04
+ */
 public class MarketAnalyzer {
 }
