d059411d59ce56152e93f68df075a283b0385425
Omar Almishri <omaralmishri56@gmail.com>
2025-03-27 02:49:52 -0600

    initialized filemanager class

--- stocktrackerproject-master/src/main/java/stocktracker/util/FileManager.java
+++ stocktrackerproject-master/src/main/java/stocktracker/util/FileManager.java
@@ -1,4 +1,25 @@
 package stocktracker.util;
 
+import stocktracker.model.*;
+import stocktracker.service.StockMarket;
+
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Class for handling file I/O operations
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 2.0
+ * @Tutorial T04
+ */
+
 public class FileManager {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
 }
