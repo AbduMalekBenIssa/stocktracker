ebce8748c943b82b6636a3aeafd012605a046d85
Omar Almishri <omaralmishri56@gmail.com>
2025-03-27 02:04:58 -0600

    imports and watchlist constructor using map/hashmap (

--- stocktrackerproject-master/src/main/java/stocktracker/model/Watchlist.java
+++ stocktrackerproject-master/src/main/java/stocktracker/model/Watchlist.java
@@ -1,4 +1,30 @@
 package stocktracker.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+/**
+ * Class representing a user's stock watchlist
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 2.0
+ * @Tutorial T04
+ */
+
+
 public class Watchlist {
+
+    private Map<String, WatchlistStock> stocks;
+
+
+    /**
+     * Constructor for the Watchlist class
+     */
+    public Watchlist() {
+        this.stocks = new HashMap<>();
+    }
+
 }
