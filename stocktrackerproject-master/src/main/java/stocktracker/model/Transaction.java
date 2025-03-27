074f2f1559740efd2d046cc20d7359bcc6636e5a
Omar Almishri <omaralmishri56@gmail.com>
2025-03-27 01:35:41 -0600

    abstract transaction class

--- stocktrackerproject-master/src/main/java/stocktracker/model/Transaction.java
+++ stocktrackerproject-master/src/main/java/stocktracker/model/Transaction.java
@@ -1,4 +1,14 @@
 package stocktracker.model;
 
-public class Transaction {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public abstract class Transaction {
+
+    protected String symbol;
+    protected int quantity;
+    protected double price;
+    protected LocalDateTime timestamp;
+
 }
