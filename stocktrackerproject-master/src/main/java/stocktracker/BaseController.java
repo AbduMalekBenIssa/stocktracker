5b9984477b0a32a1a163357ab73d4f685ea2f2fe
Omar Almishri <omaralmishri56@gmail.com>
2025-04-17 01:23:52 -0600

    initialized the base controller (rest will inherit from)

--- stocktrackerproject-master/src/main/java/stocktracker/BaseController.java
+++ stocktrackerproject-master/src/main/java/stocktracker/BaseController.java
@@ -1,4 +1,36 @@
 package stocktracker;
 
-public class BaseController {
+import stocktracker.model.User;
+import stocktracker.service.StockMarket;
+
+/**
+ * Base controller class that other view controllers will inherit from
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 3.0
+ * @Tutorial T04
+ */
+
+public abstract class BaseController {
+
+    protected User user;
+    protected StockMarket stockMarket;
+    protected MainViewController mainController;
+
+    /**
+     * Initializes the controller with user, stock market service, and main controller
+     *
+     * @param user The user
+     * @param stockMarket The stock market service
+     * @param mainController The main view controller
+     */
+    public void initialize(User user, StockMarket stockMarket, MainViewController mainController) {
+        this.user = user;
+        this.stockMarket = stockMarket;
+        this.mainController = mainController;
+
+        // Call the subclass-specific initialization
+        onInitialize();
+    }
+
 }
