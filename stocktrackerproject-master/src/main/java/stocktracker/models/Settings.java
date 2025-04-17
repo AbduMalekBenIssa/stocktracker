113e99b7ee1014186fc5ba646cc05a941fd57d98
Omar Almishri <omaralmishri56@gmail.com>
2025-04-17 01:52:55 -0600

    settings model class initialized (revamped from filemanager class)

--- stocktrackerproject-master/src/main/java/stocktracker/models/Settings.java
+++ stocktrackerproject-master/src/main/java/stocktracker/models/Settings.java
@@ -1,4 +1,40 @@
 package stocktracker.models;
 
+import javafx.beans.property.*;
+
+/**
+ * Model class for application settings.
+ */
 public class Settings {
+
+    private final StringProperty userName = new SimpleStringProperty();
+    private final StringProperty currency = new SimpleStringProperty();
+
+    // Default constructor - values will be set by SettingsService
+    public Settings() {
+    }
+
+    public String getUserName() {
+        return userName.get();
+    }
+
+    public StringProperty userNameProperty() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName.set(userName);
+    }
+
+    public String getCurrency() {
+        return currency.get();
+    }
+
+    public StringProperty currencyProperty() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency.set(currency);
+    }
 }
