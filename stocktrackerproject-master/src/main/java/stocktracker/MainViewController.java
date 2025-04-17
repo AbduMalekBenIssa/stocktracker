d36218893c1d3c15739e26d86ba048ccbd23b5eb
Omar Almishri <omaralmishri56@gmail.com>
2025-04-17 01:33:39 -0600

    imports and variable initialization

--- stocktrackerproject-master/src/main/java/stocktracker/MainViewController.java
+++ stocktrackerproject-master/src/main/java/stocktracker/MainViewController.java
@@ -1,4 +1,41 @@
 package stocktracker;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import stocktracker.model.User;
+import stocktracker.service.StockMarket;
+
+import java.io.IOException;
+
+/**
+ * Controller for the main view of the Stock Tracker application
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 3.0
+ * @Tutorial T04
+ */
 public class MainViewController {
+
+    @FXML
+    private BorderPane mainBorderPane;
+
+    @FXML
+    private StackPane contentArea;
+
+    @FXML
+    private Label userNameLabel;
+
+    @FXML
+    private Label balanceLabel;
+
+    @FXML
+    private Label totalValueLabel;
+
+    private User user;
+    private StockMarket stockMarket;
 }
