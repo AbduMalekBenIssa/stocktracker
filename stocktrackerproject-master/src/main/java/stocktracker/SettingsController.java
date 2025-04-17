7410de1b839336d9e00d3fdc36124d912294f205
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-04-16 23:26:23 -0600

    javadoc for settings

--- stocktrackerproject-master/src/main/java/stocktracker/SettingsController.java
+++ stocktrackerproject-master/src/main/java/stocktracker/SettingsController.java
@@ -5,5 +5,16 @@ import stocktracker.model.User;
 import stocktracker.services.SettingsService;
 import stocktracker.util.FileManager;
 
-public class SettingsController {
+import java.io.IOException;
+
+/**
+ * Controller for the Settings view.
+ * Manages user info and triggers data load/save.
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 3.3
+ * @Tutorial T04
+ */
+public class SettingsController extends BaseController {
+
 }
