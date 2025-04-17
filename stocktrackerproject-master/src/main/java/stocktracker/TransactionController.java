b056f8fb922bad8cd9efd6daa750ec6a2329f036
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-04-17 00:07:25 -0600

    all imports needed for transcation

--- stocktrackerproject-master/src/main/java/stocktracker/TransactionController.java
+++ stocktrackerproject-master/src/main/java/stocktracker/TransactionController.java
@@ -1,4 +1,14 @@
 package stocktracker;
 
-public class TransactionController {
-}
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import stocktracker.model.BuyTransaction;
+import stocktracker.model.SellTransaction;
+import stocktracker.model.Transaction;
