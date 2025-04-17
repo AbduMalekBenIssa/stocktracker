09677d3236034db2b1407ebb696a52ed1be2ab6a
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-04-16 23:25:59 -0600

    imports for settings controller

--- stocktrackerproject-master/src/main/java/stocktracker/SettingsController.java
+++ stocktrackerproject-master/src/main/java/stocktracker/SettingsController.java
@@ -1,4 +1,9 @@
-package stocktracker;
+import javafx.scene.control.*;
+import stocktracker.BaseController;
+import stocktracker.models.Settings;
+import stocktracker.model.User;
+import stocktracker.services.SettingsService;
+import stocktracker.util.FileManager;
 
 public class SettingsController {
 }
