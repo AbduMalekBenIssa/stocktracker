e4dfa662c57113e76e84d4543708ace49b5b8a61
Omar Almishri <omaralmishri56@gmail.com>
2025-04-17 01:28:33 -0600

    imports, initializing class variables

--- stocktrackerproject-master/src/main/java/stocktracker/DashboardController.java
+++ stocktrackerproject-master/src/main/java/stocktracker/DashboardController.java
@@ -1,4 +1,62 @@
 package stocktracker;
 
-public class DashboardController {
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import stocktracker.analysis.PortfolioAnalyzer;
+import stocktracker.model.OwnedStock;
+import stocktracker.model.Transaction;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Controller for the Dashboard view
+ *
+ * @author Omar Almishri, AbduMalek Ben Issa
+ * @version 3.0
+ * @Tutorial T04
+ */
+public class DashboardController extends BaseController {
+
+    @FXML
+    private Label balanceLabel;
+
+    @FXML
+    private Label portfolioValueLabel;
+
+    @FXML
+    private Label totalValueLabel;
+
+    @FXML
+    private Label profitLossLabel;
+
+    @FXML
+    private PieChart assetAllocationChart;
+
+    @FXML
+    private LineChart<String, Number> portfolioHistoryChart;
+
+    @FXML
+    private VBox topStocksContainer;
+
+    @FXML
+    private VBox recentTransactionsContainer;
+
+    private PortfolioAnalyzer portfolioAnalyzer;
+
+    @Override
+    protected void onInitialize() {
+        this.portfolioAnalyzer = new PortfolioAnalyzer(user, stockMarket);
+
+        // Update dashboard data
+        updateDashboardData();
+    }
 }
