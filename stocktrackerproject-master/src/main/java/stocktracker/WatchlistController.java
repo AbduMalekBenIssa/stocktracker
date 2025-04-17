35b665e7785d0b67f05b1974f0224c6965791a3d
AbduMalek <abdumalek.benissa@ucalgary.ca>
2025-04-17 00:12:16 -0600

    imports

--- stocktrackerproject-master/src/main/java/stocktracker/WatchlistController.java
+++ stocktrackerproject-master/src/main/java/stocktracker/WatchlistController.java
@@ -1,4 +1,11 @@
 package stocktracker;
 
-public class WatchlistController {
-}
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import stocktracker.model.OwnedStock;
+import stocktracker.model.WatchlistStock;
